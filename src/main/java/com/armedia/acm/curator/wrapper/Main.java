ae3cc31c9a27378f8ddd05e2cdc6f355b66410ff
diego <diego.rivera@armedia.com>
2023-07-02 17:58:30 -0600

    More progress. Added licenses.

--- src/main/java/com/armedia/acm/curator/wrapper/Main.java
+++ src/main/java/com/armedia/acm/curator/wrapper/Main.java
@@ -1,3 +1,29 @@
+/*-
+ * #%L
+ * acm-curator-wrapper
+ * %%
+ * Copyright (C) 2023 ArkCase LLC
+ * %%
+ * This file is part of the ArkCase software.
+ *
+ * If the software was purchased under a paid ArkCase license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
+ *
+ * ArkCase is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ArkCase is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ArkCase. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 package com.armedia.acm.curator.wrapper;
 
 import java.io.File;
@@ -17,8 +43,8 @@ import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.representer.Representer;
 
-import com.armedia.acm.curator.wrapper.conf.Cfg;
 import com.armedia.acm.curator.wrapper.conf.SessionCfg;
+import com.armedia.acm.curator.wrapper.conf.WrapperCfg;
 import com.armedia.acm.curator.wrapper.tools.Tools;
 
 public class Main
@@ -44,6 +70,40 @@ public class Main
         }
         Main.LOG.trace("Default config: [{}]", f);
         DEFAULT_CONFIG = f;
+    }
+
+    public static class Cfg
+    {
+        private SessionCfg session = new SessionCfg();
+        private WrapperCfg wrapper = new WrapperCfg();
+
+        public SessionCfg getSession()
+        {
+            if (this.session == null)
+            {
+                this.session = new SessionCfg();
+            }
+            return this.session;
+        }
+
+        public void setSession(SessionCfg session)
+        {
+            this.session = Tools.ifNull(this.session, SessionCfg::new);
+        }
+
+        public WrapperCfg getWrapper()
+        {
+            if (this.wrapper == null)
+            {
+                this.wrapper = new WrapperCfg();
+            }
+            return this.wrapper;
+        }
+
+        public void setWrapper(WrapperCfg wrapper)
+        {
+            this.wrapper = Tools.ifNull(this.wrapper, WrapperCfg::new);
+        }
     }
 
     private static final Options OPTIONS = new Options() //
